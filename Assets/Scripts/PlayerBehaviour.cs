255822ba8472ad84d3966df539692495be1d04ea
Dover <patrick_dover@yahoo.ca>
2019-05-16 12:02:28 -0400

    Clamped movement to viewport

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,19 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField]
-    float shipXspeed = 6f;
+    float shipXspeed = 10f;
     [SerializeField]
-    float shipYspeed = 6f;
+    float shipYspeed = 10f;
+
+    float xMin = 0f;
+    float xMax = 0f;
+    float yMin = 0f;
+    float yMax = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateMoveBoundaries();
     }
+
+    private void CreateMoveBoundaries()
+    {
+        Camera mainGameCamera = Camera.main;
+        xMin = mainGameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = mainGameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = mainGameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = mainGameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+    }   
 
     // Update is called once per frame
     void Update()
@@ -24,10 +41,10 @@ public class PlayerBehaviour : MonoBehaviour
     private void MovePlayer()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * shipXspeed;
-        var newXPos = transform.position.x + deltaX;
-
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * shipYspeed;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp((transform.position.x + deltaX), xMin, xMax);
+        var newYPos = Mathf.Clamp((transform.position.y + deltaY), yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
     }
