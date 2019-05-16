2306560f895e909621439e15edf469d60a417b98
Dover <patrick_dover@yahoo.ca>
2019-05-16 11:43:08 -0400

    Set up scene, added player movement

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float shipXspeed = 6f;
+    [SerializeField]
+    float shipYspeed = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * shipXspeed;
+        var newXPos = transform.position.x + deltaX;
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * shipYspeed;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
