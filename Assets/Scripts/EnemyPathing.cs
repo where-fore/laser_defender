d8419abc2bf76d60b61989099c9408c575dd2c8a
Dover <patrick_dover@yahoo.ca>
2019-05-16 14:02:51 -0400

    Added enemy movement framework

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> waypoints = null;
+    [SerializeField]
+    private float movespeed = 2f;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = movespeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex ++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
