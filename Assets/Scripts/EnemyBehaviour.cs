94161ebcf432746491330565153ed4b205d96fe9
Dover <patrick_dover@yahoo.ca>
2019-05-16 21:52:17 -0400

    Added enemy damage functionality

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private float health = 100;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damagedealer = other.gameObject.GetComponent<DamageDealer>();
+
+        TakeDamage(damagedealer.GetDamageValue());
+
+        damagedealer.Hit();
+    }
+
+    private void TakeDamage(float damageTaken)
+    {
+        health -= damageTaken;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/ObjectShredder.cs
+++ Assets/Scripts/ObjectShredder.cs
@@ -6,7 +6,6 @@ public class ObjectShredder : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
         Destroy(collision.gameObject);
     }
 }
