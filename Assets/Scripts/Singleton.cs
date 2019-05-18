bca16906d615ff609375318878a6bdcd0bd62753
Dover <patrick_dover@yahoo.ca>
2019-05-18 10:24:41 -0400

    Added singleton manager for music, preparing to add score

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//https://books.google.ca/books?id=W8yPDwAAQBAJ&pg=PA63&lpg=PA63&dq=unity+singleton+parent&source=bl&ots=IFeaGDvRll&sig=ACfU3U2A9oWkEwdJeYuFZxDV5Rn0Cv5acQ&hl=en&sa=X&ved=2ahUKEwiMsrHbhaXiAhVSdt8KHYiIBdMQ6AEwCXoECAkQAQ#v=onepage&q=unity%20singleton%20parent&f=false
+
 public class Singleton : MonoBehaviour
 {
     private void Awake()
