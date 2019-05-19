62a6c0de2e18fad9386f1f8ed5a934ac70f8f224
Dover <patrick_dover@yahoo.ca>
2019-05-19 11:22:10 -0400

    finalized project

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] private float spinValue = 1f;
+    void Update()
+    {
+       transform.Rotate(new Vector3(0,0, spinValue * Time.deltaTime));
+    }
+}
